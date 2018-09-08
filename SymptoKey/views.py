b5e32265d79a71d9a89636355685970f41a5aee1
kevin-warshaw <31418628+kevin-warshaw@users.noreply.github.com>
2018-09-08 01:51:06 -0400

    Added base logic behind blocks

--- SymptoKey/views.py
+++ SymptoKey/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+
+isMale = False
+age = 0
+
+
 # Create your views here.
+def UpdateChoices(request):
+    block
+    if (block.id ==):
+        block.StoreInfo()
+        block.Hide()
+        block.DisplayNext()
+
+# Update relevant variables
+def StoreInfo():
+
+
+# Mark the block itself and all associated blocks as hidden
+def Hide():
+
+# If following blocks not empty, mark them as not hidden
+def DisplayNextBlocks():
+
+
+# redirect to HTML page that tells patient to call 
+def DisplayEmergencyPage():
